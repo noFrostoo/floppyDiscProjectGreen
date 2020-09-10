ec1c21b266c3afda3a7dd41757b732b78884772e
noFrostoo <nofrost1a@gmail.com>
2020-09-10 11:52:31 +0200

    Added interface for range arms

---
+++ Assets/Scripts/RangeWeaponBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FloppyDiscProjectGreen
+{
+namespace CombatSystem
+{
+public interface IRangeWeaponBase
+{
+    void Fire(GameCharacter target);
+
+    int Reload();
+
+    int GetMagazine();
+
+    int GetDamage();
+
+    int GetFireRate();
+
+    int GetDps();
+
+    int GetCurrentAmmoInMagazine();
+
+    GameObject GetProjectile();
+
+    void ChangeFireMode(FireMode @enum);
+}
+
+
+public enum FireMode
+{
+    Automatic,
+    Burst,
+    Semi_Automatic,
+    Custom,    
+}
+
+}
+}
